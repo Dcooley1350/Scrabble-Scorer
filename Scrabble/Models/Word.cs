d39143172eb0b85ced04f3af5b8523f4784d9a78
Student <example@example.com>
2019-10-03 14:06:50 -0700

    Refactor project to use Dictionary and only one method

--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Scrabble.Models
 {
@@ -7,6 +9,7 @@ namespace Scrabble.Models
         public string MasterWord { get; set; }
         public List<char> MasterList { get; set; }
         public int WordScore { get; set; }
+        public Dictionary<int,char[]> LetterScores { get; set; }
         public char[] OnePoint { get; }
         public char[] TwoPoint { get; }
         public char[] ThreePoint { get; }
@@ -21,121 +24,33 @@ namespace Scrabble.Models
             MasterWord = masterWord;
             MasterList = new List<char>();
             WordScore = 0;
-            OnePoint = new char[] {'a','e','i','o','u','l','n','r','s','t'};
-            TwoPoint = new char[]{'d','g'};
-            ThreePoint = new char[] {'b','c','m','p'};
-            FourPoint = new char[] {'f','h','v','w','y'};
-            FivePoint = new char[] {'k'};
-            EightPoint = new char[] {'j','x'};
-            TenPoint = new char[]{'q','z'};
+            LetterScores = new Dictionary<int, char[]>();
+            LetterScores.Add(1,new char[]{'a','e','i','o','u','l','n','r','s','t'});
+            LetterScores.Add(2,new char[]{'d','g'});
+            LetterScores.Add(3,new char[]{'b','c','m','p'});
+            LetterScores.Add(4,new char[]{'f','h','v','w','y'});
+            LetterScores.Add(5,new char[]{'k'});
+            LetterScores.Add(8,new char[]{'j','x'});
+            LetterScores.Add(10,new char[]{'q','z'});
         }
         public char[] ToCharArray()
         {
             return MasterWord.ToCharArray();
         }
-        public void AwardOnePoint(char[] charArray)
+
+        public void AwardPoints(char[] charArray)
         {
             foreach(char item in charArray)
             {
-                foreach(char point in OnePoint)
+                foreach (KeyValuePair<int, char[]> entry in LetterScores)
                 {
-                    if(item == point)
+                    if( entry.Value.Contains(item))
                     {
-                        WordScore++;
-                    }
-                }
-            }
-        }
-        public void AwardPoints(char[] array)
-        {
-            AwardOnePoint(array);
-            AwardTwoPoint(array);
-            AwardThreePoint(array);
-            AwardFourPoint(array);
-            AwardFivePoint(array);
-            AwardEightPoint(array);
-            AwardTenPoint(array);
-        }
-       public void AwardTwoPoint(char[] charArray)
-        {
-            foreach(char item in charArray)
-            {
-                foreach(char point in TwoPoint)
-                {
-                    if(item == point)
-                    {
-                        WordScore +=2;
-                    }
-                }
-            }
-        }
-       public void AwardThreePoint(char[] charArray)
-        {
-            foreach(char item in charArray)
-            {
-                foreach(char point in ThreePoint)
-                {
-                    if(item == point)
-                    {
-                        WordScore +=3;
-                    }
-                }
-            }
-        }
-       public void AwardFourPoint(char[] charArray)
-        {
-            foreach(char item in charArray)
-            {
-                foreach(char point in FourPoint)
-                {
-                    if(item == point)
-                    {
-                        WordScore +=4;
-                    }
-                }
-            }
-        }
-       public void AwardFivePoint(char[] charArray)
-        {
-            foreach(char item in charArray)
-            {
-                foreach(char point in FivePoint)
-                {
-                    if(item == point)
-                    {
-                        WordScore +=5;
-                    }
-                }
-            }
-        }
-       public void AwardEightPoint(char[] charArray)
-        {
-            foreach(char item in charArray)
-            {
-                foreach(char point in EightPoint)
-                {
-                    if(item == point)
-                    {
-                        WordScore +=8;
-                    }
-                }
-            }
-        }
-       public void AwardTenPoint(char[] charArray)
-        {
-            foreach(char item in charArray)
-            {
-                foreach(char point in TenPoint)
-                {
-                    if(item == point)
-                    {
-                        WordScore +=10;
+                        WordScore += entry.Key;
                     }
                 }
             }
         }
 
-    }   
-
-
+    }
 }
